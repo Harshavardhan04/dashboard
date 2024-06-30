4356d135fdebefe6c4ec67e82f7afe02d9dc36bc
Harsh <zcabhmi@ucl.ac.uk>
2024-06-30 18:24:54 +0100

    complete frontend

---
+++ src/Components/Topbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Topbar.js
+
+import React from 'react';
+import '../Styles/Topbar.css';
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="menu">
+        <div className="menu-item">XVA</div>
+        <div className="menu-item">CVA</div>
+        <div className="menu-item">FVA</div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
